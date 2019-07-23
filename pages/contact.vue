996c4de5fc0b41e7966f1813e3bdc74047f15608
Jason Tame <jason@pango.studio>
2019-07-23 17:01:13 +0200

    Add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <Hero image="guybuttery_3.jpg" title="Contact" />
+    <div class="section is-medium contact-form">
+      <div class="columns">
+        <div class="column is-half">
+          <div class="field">
+            <label class="label">First Name</label>
+            <div class="control">
+              <input class="input" type="text" placeholder />
+            </div>
+          </div>
+        </div>
+        <div class="column is-half">
+          <div class="field">
+            <label class="label">Last Name</label>
+            <div class="control">
+              <input class="input" type="text" placeholder />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input class="input" type="email" placeholder="Email input" value="hello@" />
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Subject</label>
+        <div class="control">
+          <input class="input" type="text" placeholder value />
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Textarea"></textarea>
+        </div>
+      </div>
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-primary">Submit</button>
+        </div>
+        <div class="control">
+          <button class="button is-text">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hero from '~/components/Hero.vue'
+
+export default {
+  components: {
+    Hero
+  }
+}
+</script>
+
+<style scoped>
+@media only screen and (min-width: 1024px) {
+  .contact-form {
+    margin: 0rem 5rem;
+  }
+}
+</style>
